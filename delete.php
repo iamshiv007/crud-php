e5d66b6320ac3be9ff62fb05655beeb52b529cfe
iamshiv007 <iamshiv20032003@gmail.com>
2023-08-27 17:07:22 +0530

    delete, update and view

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+
+include("main.php");
+
+if (isset($_GET['id'])) {
+
+    $user_id = $_GET['id'];
+
+    $sql = "DELETE FROM `users` WHERE `id`='$user_id'";
+
+    $result = $conn->query($sql);
+
+    if ($result == true) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error:" . $sql . "<br>" . $conn->error;
+    }
+}
+?>
